be28a1320a395e8966aef710497f5854396ac13b
UWMDR <drowe@uwm.com>
2022-05-23 13:18:05 -0400

    changed images

--- src/data.js
+++ src/data.js
@@ -3,34 +3,30 @@ export default [
 		id: 1,
 		title: 'Weekend Beach Trip',
 		date: 'Monday, May 30, 2022',
-		image: 'https://photos.bringfido.com/photo/2011/06/12/P6110008.jpg',
+		image: 'https://images.dog.ceo/breeds/spaniel-cocker/n02102318_89.jpg',
 	},
 	{
 		id: 2,
-		title: 'Gages Birthday',
+		title: 'Happy Birthday',
 		date: 'Thursday,July 10, 2022',
-		image:
-			'https://www.thelabradorsite.com/wp-content/uploads/2016/05/White-Labradors-LS-long.jpg',
+		image: 'https://images.dog.ceo/breeds/weimaraner/n02092339_3979.jpg',
 	},
 	{
 		id: 3,
 		title: 'Weekend Hike',
 		date: 'Saturday, July 30, 2022',
-		image:
-			'https://www.thelabradorsite.com/wp-content/uploads/2019/06/Labrador-colour-inheritance-LS-long-696x377.jpg',
+		image: 'https://images.dog.ceo/breeds/tervuren/yoda_in_car.JPG',
 	},
 	{
 		id: 4,
 		title: 'Learn to Swim Weekend',
 		date: 'Friday, August 20, 2022',
-		image:
-			'https://www.thelabradorsite.com/wp-content/uploads/2020/12/can-labradors-swim-LS-long.jpg',
+		image: 'https://images.dog.ceo/breeds/malinois/n02105162_2873.jpg',
 	},
 	{
 		id: 5,
 		title: 'Doggie Dance Day',
 		date: 'Wednesday,August 31 2022',
-		image:
-			'https://www.thelabradorsite.com/wp-content/uploads/2018/06/Do-Dogs-Like-Music-or-Would-They-Rather-You-Turned-Down-the-Volume_-LS-long-696x377.jpg',
+		image: 'https://images.dog.ceo/breeds/spaniel-cocker/n02102318_10703.jpg',
 	},
 ];
